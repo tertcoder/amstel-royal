c4d828e36ed1de75035d6d42658f6d9c8585d6ae
tertcoder <tuyishimirebt12@gmail.com>
2024-07-08 02:03:55 +0200

    Update Home.tsx

--- src/Screen/Home.tsx
+++ src/Screen/Home.tsx
@@ -1,12 +1,9 @@
-import { twMerge } from "tailwind-merge";
 import FeaturedAds from "../features/home/FeaturedAds";
 import Overview from "../features/home/Overview";
 import SpecialPromotions from "../features/home/SpecialPromotions";
 import Welcome from "../features/home/Welcome";
-import useLogout from "../hooks/useLogout";
 
 function Home() {
-  const { isLoggingout } = useLogout();
 
   return (
     <div className="h-full overflow-y-auto">
