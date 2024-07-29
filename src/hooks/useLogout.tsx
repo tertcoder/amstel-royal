3ff1fa7da4eb5c4dadb73dd928205e31789907ca
tertcoder <tuyishimirebt12@gmail.com>
2024-07-29 03:43:24 +0200

    Update useLogout.tsx

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -12,6 +12,7 @@ function useLogout() {
       toast.success("You are logged out!");
       queryClient.removeQueries();
       settingLogout();
+      localStorage.clear();
     },
     onError: () => {
       toast.error("An error occurred while logging out.");
